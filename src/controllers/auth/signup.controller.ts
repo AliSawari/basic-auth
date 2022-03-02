b0906a0135d173820e397333ea5b57e7e37e2b47
Ali Sawari <ali.dragon.swr@gmail.com>
2022-03-02 20:27:23 +0300

    only returning a message instead of user obj

--- src/controllers/auth/signup.controller.ts
+++ src/controllers/auth/signup.controller.ts
@@ -4,7 +4,7 @@ import validateEmail from "../../helpers/validateEmail";
 import { generateHash } from "../../helpers/generateHash";
 import { User } from "../../models/User";
 
-export async function signUpController(req:Request, res:Response) {
+export async function signUpController(req: Request, res: Response) {
   try {
     const { fullName, email, password } = req.body;
     if (validateEmail(email)) {
@@ -13,8 +13,7 @@ export async function signUpController(req:Request, res:Response) {
       if (scorePassword(password) >= 70) {
         const hashed = await generateHash(password);
         let newUser = await new User({ fullName, email, password: hashed }).save();
-        delete newUser.password;
-        res.json({ user: newUser });
+        res.json({ message: "User Saved Successfully" });
       } else res.json({ message: "Password must be at least 8 characters and  contain lower case, upper case, numbers and special characters" })
     } else res.json({ message: "Email is not valid, Please provide a valid email address" })
   } catch (e) {
