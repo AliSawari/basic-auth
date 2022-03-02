ce72477ae5388b46d61d0e0208ec8b7f2e501c7d
Ali Sawari <ali.dragon.swr@gmail.com>
2022-03-02 19:11:23 +0300

    better functionality for the makeRequired helper

--- src/middlewares/makeRequired.ts
+++ src/middlewares/makeRequired.ts
@@ -1,15 +1,21 @@
 import { NextFunction, Request, Response } from "express";
 
-export default function makeRequired(requiredParams: object) {
+export default function makeRequired(requiredParams: string[]) {
+  const required:any = {};
+  if(requiredParams && requiredParams.length) {
+    for(let req of requiredParams) required[req] = 1;
+  }
+
   return async function (req: Request, res: Response, next: NextFunction) {
+    console.log(req.body)
     if (req.body && Object.keys(req.body).length) {
       const missing = [];
-      for (let required in requiredParams) {
-        if (!(required in req.body)) missing.push(required);
+      for (let r in required) {
+        if (!(r in req.body)) missing.push(r);
       }
       if (missing.length) {
         res.json({
-          message: `Missing Required Params`,
+          message: `Missing Required Body Params`,
           missing
         })
       } else next()
